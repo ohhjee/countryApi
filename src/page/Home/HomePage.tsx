a3dfd5f523da84c794a3bece3802cac4a52ffca3
ohhjee <orotunjigbega@gmail.com>
2023-06-04 17:29:23 +0100

    done

--- src/page/Home/HomePage.tsx
+++ src/page/Home/HomePage.tsx
@@ -25,6 +25,8 @@ export const HomePage: React.FC = () => {
   const [country, setCountry] = useState<Array<CountryId>>([]);
 
   const [search, setSearch] = useState<string | number>("");
+  const [Loader, setLoader] = useState(false);
+
   const HandleSearch = async () => {
     try {
       const { data } = await axios(
@@ -78,8 +80,11 @@ export const HomePage: React.FC = () => {
   };
 
   const fetchCountry = async () => {
+    setLoader(true);
+
     const { data } = await axios.get(" https://restcountries.com/v3.1/all ");
     setCountry(data);
+    setLoader(false);
   };
   useEffect(() => {
     fetchCountry();
@@ -87,98 +92,124 @@ export const HomePage: React.FC = () => {
 
   return (
     <>
-      <div>
+      {Loader ? (
+        <div role="status" className="flex items-center justify-center">
+          <svg
+            aria-hidden="true"
+            className="w-8 h-8 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+            viewBox="0 0 100 101"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+              fill="currentColor"
+            />
+            <path
+              d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+              fill="currentFill"
+            />
+          </svg>
+          <span className="sr-only">Loading...</span>
+        </div>
+      ) : (
         <div>
-          <div className="flex flex-col sm:flex-row space-y-6 sm:space-y-0 justify-between items-center my-[2rem] px-2 md:px-0">
-            <div className="w-full sm:w-4/12">
-              <form className="" onSubmit={HandleSearchForm}>
-                <div className="relative h-full">
-                  <input
-                    autoComplete=""
-                    type="text"
-                    name="search"
-                    onChange={(event) => setSearch(event.target.value)}
-                    className="w-full dark:bg-slate-700  shadow-[0_0_5px_2px] shadow-slate-500 pl-[1.5rem] focus-within:outline-none px-2 text-[.8rem] h-[5vh] "
-                    value={search}
-                    placeholder="search for a country"
-                  />
-                  <div className="absolute top-0 flex items-center h-full left-1">
-                    <MagnifyingGlassIcon className="w-4 h-4" />
+          <div>
+            <div className="flex flex-col sm:flex-row space-y-6 sm:space-y-0 justify-between items-center my-[2rem] px-2 md:px-0">
+              <div className="w-full sm:w-4/12">
+                <form className="" onSubmit={HandleSearchForm}>
+                  <div className="relative h-full">
+                    <input
+                      autoComplete=""
+                      type="text"
+                      name="search"
+                      onChange={(event) => setSearch(event.target.value)}
+                      className="w-full dark:bg-slate-700  shadow-[0_0_5px_2px] shadow-slate-500 pl-[1.5rem] focus-within:outline-none px-2 text-[.8rem] h-[5vh] "
+                      value={search}
+                      placeholder="search for a country"
+                    />
+                    <div className="absolute top-0 flex items-center h-full left-1">
+                      <MagnifyingGlassIcon className="w-4 h-4" />
+                    </div>
                   </div>
-                </div>
-              </form>
-            </div>
-            <div className="w-full sm:w-4/12">
-              <form onChange={HandleFormFiter}>
-                <select
-                  name=""
-                  id=""
-                  onChange={(e) => HandleFilter(e.target.value)}
-                  className="w-full dark:bg-slate-700 shadow-[0_0_5px_2px] shadow-slate-500 h-[5vh] bg-white px-2 focus-within:outline-none"
-                >
-                  {regions.map((region, index) => (
-                    <option key={index} value={region.name} className="rounded">
-                      {region.name}
-                    </option>
-                  ))}
-                </select>
-              </form>
+                </form>
+              </div>
+              <div className="w-full sm:w-4/12">
+                <form onChange={HandleFormFiter}>
+                  <select
+                    name=""
+                    id=""
+                    onChange={(e) => HandleFilter(e.target.value)}
+                    className="w-full dark:bg-slate-700 shadow-[0_0_5px_2px] shadow-slate-500 h-[5vh] bg-white px-2 focus-within:outline-none"
+                  >
+                    {regions.map((region, index) => (
+                      <option
+                        key={index}
+                        value={region.name}
+                        className="rounded"
+                      >
+                        {region.name}
+                      </option>
+                    ))}
+                  </select>
+                </form>
+              </div>
             </div>
           </div>
-        </div>
 
-        <div>
-          <>
-            <div className="flex gap-[2rem] flex-wrap ">
-              {country.map((res: CountryId, index) => (
-                <div
-                  key={index}
-                  className="  flex-[1_1_50%]  xs:flex-[1_1_30%] lg:flex-[1_1_20%] shadow-gray-400 shadow-[0_0_5px_2px] gap-[2rem] rounded"
-                >
-                  <Link to={`country/${res.name.common}`}>
-                    <div>
-                      <div className="h-[20vh] ">
-                        <img
-                          src={res.flags.png}
-                          alt=""
-                          className="w-full h-full rounded-t"
-                        />
+          <div>
+            <>
+              <div className="flex gap-[2rem] flex-wrap ">
+                {country.map((res: CountryId, index) => (
+                  <div
+                    key={index}
+                    className="  flex-[1_1_50%]  xs:flex-[1_1_30%] lg:flex-[1_1_20%] shadow-gray-400 shadow-[0_0_5px_2px] gap-[2rem] rounded"
+                  >
+                    <Link to={`country/${res.name.common}`}>
+                      <div>
+                        <div className="h-[20vh] ">
+                          <img
+                            src={res.flags.png}
+                            alt=""
+                            className="w-full h-full rounded-t"
+                          />
+                        </div>
+                        <div className="p-3">
+                          <div className="font-semibold text-[.8rem]">
+                            {res.name.common}
+                          </div>
+                          <div className="">
+                            <span className="text-[.7rem] font-bold">
+                              Population:{" "}
+                            </span>
+                            <span className="text-[.7rem] ">
+                              {res.population.toLocaleString()}
+                            </span>
+                          </div>
+                          <div className="">
+                            <span className="text-[.7rem] font-bold">
+                              Region:{" "}
+                            </span>
+                            <span className="text-[.7rem] ">
+                              {res.continents}
+                            </span>
+                          </div>
+                          <div className="">
+                            <span className="text-[.7rem] font-bold">
+                              Capital:{" "}
+                            </span>
+                            <span className="text-[.7rem] ">{res.capital}</span>
+                          </div>
+                        </div>
                       </div>
-                      <div className="p-3">
-                        <div className="font-semibold text-[.8rem]">
-                          {res.name.common}
-                        </div>
-                        <div className="">
-                          <span className="text-[.7rem] font-bold">
-                            Population:{" "}
-                          </span>
-                          <span className="text-[.7rem] ">
-                            {res.population.toLocaleString()}
-                          </span>
-                        </div>
-                        <div className="">
-                          <span className="text-[.7rem] font-bold">
-                            Region:{" "}
-                          </span>
-                          <span className="text-[.7rem] ">
-                            {res.continents}
-                          </span>
-                        </div>
-                        <div className="">
-                          <span className="text-[.7rem] font-bold">
-                            Capital:{" "}
-                          </span>
-                          <span className="text-[.7rem] ">{res.capital}</span>
-                        </div>
-                      </div>
-                    </div>
-                  </Link>
-                </div>
-              ))}
-            </div>
-          </>
+                    </Link>
+                  </div>
+                ))}
+              </div>
+            </>
+          </div>
         </div>
-      </div>
+      )}
     </>
   );
 };
